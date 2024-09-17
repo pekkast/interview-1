dbd6165ea84695f8d3a4848154c4e7b3c0aac345
Pekka Turtola <pekka.turtola@sap.com>
2024-09-17 15:35:43 +0300

    Some enhancements

--- src/services/recipes.ts
+++ src/services/recipes.ts
@@ -1,5 +1,3 @@
 export const fetchAll = () => {
-  return fetch("https://dummyjson.com/recipes")
-    .then((res) => res.json())
-    .then((data) => data.recipes);
+  return fetch("https://dummyjson.com/recipes").then((res) => res.json());
 };
